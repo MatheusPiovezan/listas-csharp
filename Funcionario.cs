ea46d2a7465fbb5b1be559c6eeab4b081913c70e
Matheus Piovezan <matheuspiovezan@outlook.com>
2023-08-30 10:33:11 -0300

    adding

---
+++ Funcionario.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course9Exerc
+{
+    internal class Funcionario
+    {
+        public int Id { get; private set; }
+        public string Nome { get; private set; }
+        public double Salario { get; set; }
+
+        public Funcionario(int id, string nome, double salario)
+        {
+            Id = id;
+            Nome = nome;
+            Salario = salario;
+        }
+
+        public void AumentarSalario(double p)
+        {
+            Salario += Salario * p / 100.0;
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}, {Nome}, {Salario}";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Course9Exerc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Quantos funcionarios serão registrados? ");
+            int qFunc = int.Parse(Console.ReadLine());
+
+            List<Funcionario> list = new List<Funcionario>();
+
+            for (int i = 0; i < qFunc; i++)
+            {
+                Console.Write("Insira o Id: ");
+                int id = int.Parse(Console.ReadLine());
+                Console.Write("Insira o Nome: ");
+                string nome = Console.ReadLine();
+                Console.Write("Insira o Salario: ");
+                double salario = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+                list.Add(new Funcionario(id, nome, salario));
+
+                Console.WriteLine();
+            }
+
+            Console.Write("Id do funcionario que tera o salario aumentado: ");
+            int aumentarSalarioId = int.Parse(Console.ReadLine());
+            Funcionario emp = list.Find(x => x.Id == aumentarSalarioId);
+
+            if (emp != null)
+            {
+                emp.AumentarSalario(10.0);
+            }
+            else
+            {
+                Console.Clear();
+                Console.WriteLine("Este id não existe!");
+            }
+
+            foreach (Funcionario f in list)
+            {
+                Console.WriteLine(f.ToString());
+            }
+        }
+    }
+}
+
+
